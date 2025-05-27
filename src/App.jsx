111c24e355b9b13dc31799647930fdc8baba1e9c
Vraj-Prajapati-14 <vraj.prajapati@bacancy.com>
2025-05-27 12:09:25 +0530

    [FEAT]:Husky+Eslint+Prettior+Tailwind Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Vraj</h1>
+      <h2>git commit -m "[FEAT]:Husky+Eslint+Prettior+Tailwind Setup"</h2>
+    </>
+  );
+}
+
+export default App;
